a2a517c06643ec9c043123a6d73c44dc458406a3
Kalen Hermalin <kalen.hermalin@gmail.com>
2025-03-26 16:59:45 -0400

    Refactor project structure and add CI workflow with TypeScript configuration

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { SimpleAuth } from "./lib/SimpleAuth";
-import { OAuth2Client } from "./lib/OAuth2Client";
-import { Provider } from "./lib/Provider";
-export { SimpleAuth, OAuth2Client, Provider };
-
---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+import { SimpleAuth } from "./SimpleAuth";
+import { OAuth2Client } from "./OAuth2Client";
+import { Provider } from "./Provider";
+export { SimpleAuth, OAuth2Client, Provider };
+

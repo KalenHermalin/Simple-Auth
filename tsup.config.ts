75a14024fc195f2d8cfd46ccbb9c5e804565e7d9
Kalen Hermalin <kalen.hermalin@gmail.com>
2025-04-07 17:26:35 -0400

    More fixes

--- tsup.config.ts
+++ tsup.config.ts
@@ -11,4 +11,7 @@ export default defineConfig({
     shims: true,
     skipNodeModulesBundle: true,
     clean: true,
+    esbuildOptions(options) {
+        options.conditions = ['types', 'import', 'require'];
+    }
 });

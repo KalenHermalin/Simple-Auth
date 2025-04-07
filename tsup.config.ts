9a39967690ef832c27205d31bd06d44823507729
Kalen Hermalin <kalen.hermalin@gmail.com>
2025-04-07 17:01:16 -0400

    push

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from "tsup";
 
 export default defineConfig({
     format: ['cjs', 'esm'],
-    entry: ['./lib/index.ts'],
+    entry: {
+        index: './lib/index.ts',
+        types: './lib/types.ts',
+        'providers/index': './lib/providers/index.ts'
+    },
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
     clean: true,
-})
+});
